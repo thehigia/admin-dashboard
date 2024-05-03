64c59b09eb5fd55e4663b1ffe6a5e791ba05e648
fgabrielqr <fgabrielqr@gmail.com>
2024-05-03 15:14:00 -0300

    feat: :sparkles: Concluindo layout e modal de categorias

--- src/components/Categoria/Categoria.jsx
+++ src/components/Categoria/Categoria.jsx
@@ -5,10 +5,28 @@ import ReactPaginate from 'react-paginate';
 import AddLayer from '../../assets/Add-Layer.svg';
 import Edit from '../../assets/pencil-square.svg';
 import Delete from '../../assets/trash-fill.svg';
+import Search from '../../assets/ion_search.svg';
 
 const Categoria = () => {
     const { category } = useAppContext();
     console.log("Teste", category)
+    const [showModal, setShowModal] = useState(false);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleAddClick = () => {
+        setShowModal(true);
+    };
+
+    const handleSave = () => {
+        // Aqui você pode fazer uma requisição para salvar a categoria
+        console.log('Categoria adicionada:', title, description);
+        setShowModal(false);
+    };
+
+    const handleCancel = () => {
+        setShowModal(false);
+    };
 
     const initialCategories = [
         { id: 1, title: 'Lorem Ipsum Dolor' },
@@ -60,7 +78,7 @@ const Categoria = () => {
                         <img src={AddLayer} alt='Logotipo do App' />
                         <h1>Categoria</h1>
                     </div>
-                    <a className={styles.addButton}>Adicionar +</a>
+                    <a className={styles.addButton} onClick={handleAddClick}>Adicionar +</a>
                 </div>
                 <div className={styles.controls}>
                     <div className={styles.searchArea}>
@@ -71,6 +89,7 @@ const Categoria = () => {
                             value={searchTerm}
                             onChange={(e) => setSearchTerm(e.target.value)}
                         />
+                        <img className={styles.searchIcon} src={Search} />
                     </div>
                     <div className={styles.paginationControls}>
                         <span className={styles.span}>Apresentar</span>
@@ -114,6 +133,46 @@ const Categoria = () => {
                 containerClassName={styles.pagination}
                 activeClassName={styles.activePage}
             />
+
+            {showModal && (
+                <div className={styles.modal}>
+                    <div className={styles.modalContent}>
+                        <h3 className={styles.titleModal}>Nova Categoria</h3>
+                        <p className={styles.subtitleModal}>Preencha os campos abaixo para criar uma nova categoria</p>
+                        <form className={styles.form}>
+                            <div className={styles.labMod}>
+                                <label className={styles.labelModal}>
+                                    Título
+                                </label>
+                                <input
+                                    className={styles.input}
+                                    type="text"
+                                    placeholder='Escreva o título da categoria aqui...'
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                />
+                            </div>
+                            <div className={styles.labMod}>
+                                <label className={styles.labelModal}>
+                                    Descrição
+                                </label>
+                                <textarea
+                                    className={styles.textArea}
+                                    value={description}
+                                    placeholder='Breve descrição sobre a categoria criada...'
+                                    onChange={(e) => setDescription(e.target.value)}
+                                />
+
+                            </div>
+                            <div className={styles.modalButtons}>
+                                <a className={styles.btnCancel} onClick={handleCancel}>Cancelar</a>
+                                <a className={styles.btnSave} onClick={handleSave}>Salvar</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )
+            }
         </div>
     );
 };
--- src/layouts/LayoutsPadrao/LayoutsPadrao.jsx
+++ src/layouts/LayoutsPadrao/LayoutsPadrao.jsx
@@ -1,16 +1,14 @@
 import { Outlet } from "react-router-dom"
 import { Cabecalho, Conteudo, Footer } from "../../components"
-import { useAppContext } from "../../hooks";
 
 const LayoutsPadrao = () => {
-    const { criador } = useAppContext();
     return (
         <>
-            <Cabecalho nomeUsuario="Felipe" />
+            <Cabecalho />
             <Conteudo>
                 <Outlet />
             </Conteudo>
-            <Footer nomeCriador={criador} />
+            <Footer />
         </>
     )
 }
